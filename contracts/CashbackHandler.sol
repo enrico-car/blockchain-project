e409791414dd2bfa4bd96d45b10430fcd6b5d34f
MarcoRcc <95227286+MarcoRcc@users.noreply.github.com>
2025-06-11 15:05:22 +0200

    Added cashback contracts and ignition

---
+++ contracts/CashbackHandler.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.28;
+
+import {CashbackToken} from "./CashbackToken.sol";
+
+contract CashbackHandler {
+    
+    CashbackToken public cashbackToken;
+    uint256 public constant MIN_CASHBACK_AMOUNT = 100 * 10**18;
+
+    event CashbackRedeemed ( address indexed user, uint256 amount);
+
+    constructor (address cashbackTokenAddress) {
+        cashbackToken = CashbackToken(cashbackTokenAddress);
+    }
+
+    // TODO: lo rendiamo parametrico?? Es: riscatto 200 token al posto dei 100 fissi
+    function redeem_cashback () external {
+
+        // Check if contract is allowed to spend
+        require(cashbackToken.allowance(msg.sender, address(this)) >= MIN_CASHBACK_AMOUNT, 
+            string.concat("Contract must be approved to spend at least 100 ", cashbackToken.symbol(), "."));
+
+        // Cjheck if the caller has enough token
+        require(cashbackToken.balanceOf(msg.sender) >= MIN_CASHBACK_AMOUNT, 
+            string.concat("You must have at least 100 ", cashbackToken.symbol(), " to redeem a cashback"));
+
+        // Transfer the tokens to the contract
+        cashbackToken.transferFrom(msg.sender, address(this), MIN_CASHBACK_AMOUNT);
+
+        // Burn contract's tokens
+        cashbackToken.burn(address(this), MIN_CASHBACK_AMOUNT);
+
+        // Emit event
+        emit CashbackRedeemed(msg.sender, MIN_CASHBACK_AMOUNT);
+
+    }
+
+}
+
+/*
+
+    Workflow:
+    1 - Deploy CashbackToken
+    2 - Deploy CashbackHandler, using the CashbackToken address as parameter in the constructor
+    3 - Call setBurnerAuth, using the CashbackHandler address as parameter, allowing the contract to burn tokens
+    4 - Mint some tokens (es: 1000000000000000000000000)
+    5 - Approve contract to spend some tokens (es: 100000000000000000000)
+    6 - Call redeem_cashback
+
+*/
