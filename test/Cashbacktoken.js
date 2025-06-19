5bb204a5f2a03ca1baac9da66a7643317e9c6241
Nicola Mores <51528817+NikoMrs@users.noreply.github.com>
2025-06-19 14:56:26 +0200

    Added test for burn(uint256)

--- test/Cashbacktoken.js
+++ test/Cashbacktoken.js
@@ -1,12 +1,11 @@
 const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("CashbackToken", function () {
   async function deployCashbackToken() {
-        const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, otherAccount] = await ethers.getSigners();
 
     const CashbackToken = await ethers.getContractFactory("CashbackToken");
     const cashbackToken = await CashbackToken.deploy();
@@ -16,14 +15,14 @@ describe("CashbackToken", function () {
 
   describe("Deployment", function () {
     it("Should deploy the right token", async function () {
-      const { cashbackToken, owner } = await loadFixture(deployCashbackToken);
+      const { cashbackToken } = await loadFixture(deployCashbackToken);
 
       expect(await cashbackToken.name()).to.equal("CashbackToken");
       expect(await cashbackToken.symbol()).to.equal("CSHBK");
     });
 
     it("Should start without any supply", async function () {
-      const { cashbackToken, owner } = await loadFixture(deployCashbackToken);
+      const { cashbackToken } = await loadFixture(deployCashbackToken);
 
       expect(await cashbackToken.totalSupply()).to.equal(0);
     });
@@ -36,6 +35,13 @@ describe("CashbackToken", function () {
   });
 
   describe("Access Control Lists", function () {
+    it("Should prevent unauthorized changes to the ACLs", async function () {
+      const { cashbackToken, owner, otherAccount } = await loadFixture(deployCashbackToken);
+
+      await expect(cashbackToken.connect(otherAccount).setBurnerAuth(owner.address, true)).to.be.reverted;
+      await expect(cashbackToken.connect(otherAccount).setMinterAuth(owner.address, true)).to.be.reverted;
+    });
+
     it("Should allow the owner to modify the ACLs", async function () {
       const { cashbackToken, owner, otherAccount } = await loadFixture(deployCashbackToken);
 
@@ -94,9 +100,32 @@ describe("CashbackToken", function () {
       expect(await cashbackToken.balanceOf(owner.address)).to.equal(mintAmount - burnAmount);
     });
 
+    it("Should allow an user to burn its own tokens", async function () {
+      const { cashbackToken, owner } = await loadFixture(deployCashbackToken);
+
+      const initialBalance = await cashbackToken.balanceOf(owner.address);
+      expect(initialBalance).to.equal(0);
+
+      const mintAmount = 1000;
+      await cashbackToken.setMinterAuth(owner.address, true);
+      await cashbackToken.mint(owner.address, mintAmount);
+
+      await cashbackToken.setBurnerAuth(owner.address, true);
+      const burnAmount = 500;
+      await cashbackToken["burn(uint256)"](burnAmount);
+
+      expect(await cashbackToken.balanceOf(owner.address)).to.equal(mintAmount - burnAmount);
+    });
     
   });
+
+  // describe("Approve and TrasferFrom operations", function () {
+  //   it("Should allow users to approve other wallets", async function () {
+  //     
+  //   });
+  // });
 
 });
 
 // TODO: La funzione burn(uint256 amount) non è stata testata, in quanto non sappiamo se mantenerla o eliminarla.
+// TODO: Aggiungiamo anche testing per approve e transferFrom?? Sono funzioni non implementate da noi, ma da openzeppelin, quindi non penso sia necessario.
