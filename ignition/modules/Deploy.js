cd77e3324497016a771a1f5f0decf5bffd847642
MarcoRcc <95227286+MarcoRcc@users.noreply.github.com>
2025-06-21 14:58:04 +0200

    Update Deploy.js

--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -11,14 +11,15 @@ module.exports = buildModule("DeployModule", (m) => {
   // Product manager
   const productManager = m.contract("ProductManager");
 
-  // Authorize the manufactoring house to create products and lots
+  // Authorize the manufactoring house to create products and lots (Creation and removal of lots and products)
   m.call(productManager, "setUserAuth", [
     "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // Change with env
     true,
   ]);
-
   // Inventory manager
   const inventoryManager = m.contract("InventoryManager", [productManager]);
+  // Give permission to the manufacturer to add lots to its own inventory
+  m.call(inventoryManager, "setManufacturerAuth", ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", true]);
 
   // Transaction manager
   const transactionManager = m.contract("TransactionManager", [
@@ -26,10 +27,11 @@ module.exports = buildModule("DeployModule", (m) => {
     inventoryManager,
     token,
   ]);
+
   // get Mint authorization to Transactin manager
   m.call(token, "setMinterAuth", [transactionManager, true]);
-  // ????
-  m.call(inventoryManager, "setManufacturerAuth", [transactionManager, true]);
+  // Give the transactionManager permission to handle users inventory 
+  m.call(inventoryManager, "setUserAuth", [transactionManager, true])
 
   return { token, handler };
 });
