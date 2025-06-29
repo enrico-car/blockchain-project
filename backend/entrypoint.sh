2e57352246d8f7cb2ef9a01c2e81cd613c3d9457
Nicola <51528817+NikoMrs@users.noreply.github.com>
2025-06-29 15:49:16 +0200

    Updated contracts' Addresses and ABIs fetch

---
+++ backend/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Backend entrypoint start"
+
+sleep 15
+
+# Timer di 60 secondi
+MAX_WAIT=60
+SECONDS_PASSED=0
+
+while [ ! -f "/app/shared/deployedContracts.json" ] && [ $SECONDS_PASSED -lt $MAX_WAIT ]; do
+    echo "⚠️ No ABI found in /app/shared/abis. Waiting..."
+    sleep 5
+    SECONDS_PASSED=$((SECONDS_PASSED + 5))
+done
+
+if [ -f "/app/shared/deployedContracts.json" ]; then
+    echo "📦 Copying ABIs..."
+    mkdir -p /app/src/utils/abis
+    cp /app/shared/abis/*.json /app/src/utils/abis/
+    cp /app/shared/deployedContracts.json /app/src/utils/deployedContracts.json
+else
+    echo "❌ Timeout: No ABI found after $MAX_WAIT seconds."
+fi
+
+npm run dev
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,23 +1,43 @@
-// deploy.js
 const hre = require("hardhat");
 const DeployModule = require("../ignition/modules/Deploy.js");
-
 const fs = require("fs");
+const path = require("path");
 
 async function main() {
   const deployment = await hre.ignition.deploy(DeployModule);
 
-  //   console.log(deployment);
+  const abisDir = "/app/shared/abis";
+  const addressesFile = "/app/shared/deployedContracts.json";
 
-  const addresses = {};
-  for (const [name, contract] of Object.entries(deployment)) {
-    addresses[name] = contract.target;
+  if (!fs.existsSync(abisDir)) {
+    fs.mkdirSync(abisDir, { recursive: true });
   }
 
-  fs.writeFileSync('/app/shared/deployedContracts.json', JSON.stringify(addresses, null, 2));
+  const addresses = {};
 
-  console.log("Deployed contracts:");
-  console.log(addresses);
+  for (const [name, contract] of Object.entries(deployment)) {
+    addresses[name] = contract.target;
+
+    try {
+      console.log(`📦 Fetching ABI for ${name}`);
+      const artifact = await hre.artifacts.readArtifact(name);
+
+      // Salva l`ABI
+      fs.writeFileSync(
+        path.join(abisDir, `${name}.json`),
+        JSON.stringify(artifact.abi, null, 2)
+      );
+    } catch (err) {
+      console.error(`❌ Error fetching ABI for ${name}:`, err);
+    }
+  }
+
+  console.log("Contract addresses: ", addresses);
+
+  // Salva gli address dei contratti
+  fs.writeFileSync(addressesFile, JSON.stringify(addresses, null, 2));
+  console.log("✅ Address saved in", addressesFile);
+  console.log("📂 ABI saved in", abisDir);
 }
 
 main().catch((error) => {
