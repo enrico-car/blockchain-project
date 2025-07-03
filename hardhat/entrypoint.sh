cfbac8d62a327c09c6b09dfb43b7dd4721e69d43
enrico-car <car.enrico19@gmail.com>
2025-07-03 15:42:57 +0200

    docker comments

--- hardhat/entrypoint.sh
+++ hardhat/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# This script is used to start the Hardhat node and run the deployment script.
 npx hardhat node --hostname 0.0.0.0 &
 
 sleep 5
 
+# run the deployment script
 npx hardhat run scripts/deploy.js --network localhost
 
+# run the tests
 npx hardhat coverage
 
+# Keep the container running
 tail -f /dev/null
